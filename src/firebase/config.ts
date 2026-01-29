ac74c937e18983076a17417fc78afe4e8e21435f
Uberdas <u59989873@gmail.com>
2026-01-29 11:11:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8396028096-d5b10",
+  "appId": "1:180514884650:web:25ce88d670ee5337db8753",
+  "apiKey": "AIzaSyCW3_ffr7Y-uYu---4uuaMPpC66VtHD_b0",
+  "authDomain": "studio-8396028096-d5b10.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "180514884650"
+};
