dc780637eae393cb27128cf8e72af9b1578c4832
Uberdas <u59989873@gmail.com>
2026-02-01 23:03:59 +0000

    Use this 100% same logo for HexaVision only and don't touch logo of Slic

--- src/components/hexavision-logo.tsx
+++ src/components/hexavision-logo.tsx
@@ -11,8 +11,9 @@ export function HexaVisionLogo({ className }: { className?: string }) {
         xmlns="http://www.w3.org/2000/svg"
         className="text-primary"
        >
-        <path d="M16.8928 2.36364L8.98863 6.31818C7.99419 6.81539 7.497 7.06399 7.15572 7.42617C6.81444 7.78835 6.66319 8.24949 6.36069 9.17177L4.75 14.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M7.10718 21.6364L15.0114 17.6818C16.0058 17.1846 16.503 16.936 16.8443 16.5738C17.1856 16.2116 17.3368 15.7505 17.6393 14.8282L19.25 9.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M17.25 4.5H6.75L2.25 12L6.75 19.5H17.25L21.75 12L17.25 4.5Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M13.5 8.25L10.5 8.25C9.39543 8.25 8.5 9.14543 8.5 10.25V13.75C8.5 14.8546 9.39543 15.75 10.5 15.75L13.5 15.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M10.5 12H15.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
        </svg>
       <span className="font-headline text-lg font-bold text-foreground tracking-wide">
         Hexa Vision
